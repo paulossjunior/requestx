0795dc837796f854cde8433687fc30dbdd494674
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-01 16:11:54 -0300

    x

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='request_x',  # Required
 
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
     packages=['request_x'],
-    install_requires=['requests'],  # Optional
+    install_requires=['request'],  # Optional
 )
